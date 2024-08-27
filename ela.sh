9cbde569570341b257c3ebb03a12cad7a3106065
bRuttaZz <agrajpdasprivate@gmail.com>
2024-08-27 23:13:35 +0530

    minor modifications

--- ela.sh
+++ ela.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-trap "tput reset; tput cnorm; exit" 2
-
 
 DICTIONARY='
 Word dictionary
@@ -13,19 +11,20 @@ For tput color codes and detailed docs : https://linuxcommand.org/lc3_adv_tput.p
 '
 
 # Globals
-UPDATE_INTERVEL=10   # in seconds
+UPDATE_INTERVEL=15   # in seconds
 GREET_COLORS=( 1 3 5 6)
 GREET_COLOR_COUNT=${#GREET_COLORS[@]}
 
 ROWS=0
 COLS=0
+MIN_ROWS=24
+MIN_COLS=62
+g_sec_counter=0
+g_loop_flag=true
 
-function update_row_col() {
-    ROWS=$(tput lines)
-    COLS=$(tput cols)
-}
-trap 'update_row_col' WINCH
 
+# try to keep the length max to `MIN_COLS-3` chars (otherwise construct a
+# function to draw multiline printing (POV: me being lazy))
 ONAM_QUOTES=(
     "Uthraadam ucchayaakumpol acchimaarkku veppraalam"
     "Kaanam vittum onam unnanam"
@@ -35,6 +34,7 @@ ONAM_QUOTES=(
     "Onam kazhinjaal oolappura oottappura"
     "Chinga maasatthile thiruvonanaalil poocchakku vayaru vedana"
 )
+# keep it under `MIN_COLS-3` char length
 AUTHORS_ASSIST=(
     "the ancient relic that just won't go extinct"
     "NB: subjected to market risks"
@@ -45,9 +45,16 @@ AUTHORS_ASSIST=(
     "sometimes it's all about privilege.."
 )
 
-for str in ${ONAM_QUOTES[@]}; do
-  echo $str
-done
+# recipe
+declare -A RECIPE=(
+    ["X"]="banana leaf"
+    ["K"]="kichadi"
+    ["P"]="pinapple kichadi"
+    ["B"]="pachadi"
+    ["E"]="ginger curry"
+    ["o"]="pappadam"
+    ["0"]="rice"
+)
 
 
 function iter_chars() {         # to draw a character repeatedly
@@ -68,7 +75,7 @@ function draw_ela() {           # by the way `ela` -> `vazha ela` -> banana leaf
 
 
     ela_b=$(($height/2));    # ela `thumb` semi minor axis
-    char_="x";
+    char_="X";
 
     tput setaf 2; tput bold ;   # font settings
 
@@ -119,7 +126,7 @@ function draw_elipse_item() {   # to draw a eliptical object
 function onam_quote() {         # to draw onam quote
     height=${1:-18}
 
-    head="Ona-Quote:"
+    head="Ona-Quote"
     tput setaf 7;
 
     padding=$((  COLS/2 - ${#head}/2 ))
@@ -128,61 +135,104 @@ function onam_quote() {         # to draw onam quote
 
     choice=$(( RANDOM % ${#ONAM_QUOTES[@]} ))
     quote=${ONAM_QUOTES[$choice]}
-    padding=$((  COLS/2 - ${#quote}/2 ))
+    padding=$(( (COLS/2 - ${#quote}/2)-1 ))
+    if ((${#quote}>=COLS)); then padding=0; fi
     tput cup $(( height + 1)) $padding;
     tput bold; echo -n \"$quote\"; tput sgr0;
 
     tput setaf 7;
-    authors_note=${AUTHORS_ASSIST[$choice]}
-    padding=$((COLS - ${#authors_note} - 1))
-    tput cup $((ROWS-2)) $padding
+    authors_note="(${AUTHORS_ASSIST[$choice]})"
+    padding=$(( (COLS/2 - ${#authors_note}/2) - 1))
+    if ((${#authors_note}>=COLS)); then padding=0; fi
+    tput cup $(( height + 2)) $padding;
     echo -n $authors_note
     tput setaf 9; tput sgr0;
 }
 
-function recipe_index() {     # prit an index table
+function print_recipe() {     # to show recipe (as you have already guessed)
+    # finding max section length
+    section_max_len=0
+    recipe_item_len="${#RECIPE[@]}"
+    section_padding=3
+    for recipe in "${!RECIPE[@]}"; do
+        rec="${RECIPE[$recipe]}"
+        len=$(( ${#rec} + ${#recipe} + 3 + $section_padding))
+        if ((len > section_max_len)); then
+            section_max_len=$len
+        fi
+    done
+
+    sections=$(( COLS / section_max_len ))
+    section_rows=$(( recipe_item_len / sections ))
+    if (( (sections * section_rows) < recipe_item_len )); then
+        ((section_rows ++))
+    fi
+
+    row=$(( ROWS - section_rows - 3 ))
+    col=2
     tput setaf 7;
-    tput cup $(( ROWS - 9 )) 0
-    tput smul; echo -n "Ona sadhya recipe" ; tput rmul;
+    tput cup $row 1; iter_chars $((COLS-2)) "─"
+    tput cup $row 0; echo -n "┌"
+    tput cup $row $COLS; echo -n "┐"
+    (( row ++ ))
+    tput cup $row $col
+    tput smul; tput bold; echo -n "Recipe" ; tput sgr0; tput rmul;
+    tput cup $row 0; echo -n "│"
+    tput cup $row $COLS; echo -n "│"
+    (( row ++ ))
 
-    tput cup $(( ROWS - 8 )) 0
-    echo -ne "x :\tbanana leaf"
+    sec=0
+    for recipe in "${!RECIPE[@]}"; do
+        tput cup $row $col
+        echo -n "$recipe: ${RECIPE[$recipe]}"
+        tput cup $row 0; echo -n "│"
+        tput cup $row $COLS; echo -n "│"
 
-    tput cup $(( ROWS - 7 )) 0
-    echo -ne "K :\tkichadi"
-
-    tput cup $(( ROWS - 6 )) 0
-    echo -ne "P :\tpinapple kichadi"
-
-    tput cup $(( ROWS - 5 )) 0
-    echo -ne "B :\tpachadi"
-
-    tput cup $(( ROWS - 4 )) 0
-    echo -ne "E :\tginger Curry"
-
-    tput cup $(( ROWS - 3 )) 0
-    echo -ne "o :\tpappadam"
-
-    tput cup $(( ROWS - 2 )) 0
-    echo -ne "0 :\trice"
+        (( sec ++))
+        if ((sec >= sections)); then
+            sec=0
+            (( row ++ ))
+            col=2
+        else
+            col=$(( col + section_max_len ))
+        fi
+    done
+    ((row ++))
+    tput cup $row 1;  iter_chars $((COLS-2)) "─"
+    tput cup $row 0; echo -n "└"
+    tput cup $row $COLS; echo -n "┘"
 
     tput setaf 9; tput sgr0;
 }
 
-function draw_onaSHadhya() {   # draw ona sadhya
+function window_size_checker() {    # check and throw a warning on windows sizing issues
+    if ((ROWS < MIN_ROWS || COLS < MIN_COLS )); then
+        >&2 echo -e "\033[0;31mOops! Looks like your terminal grid is too small ($ROWS x $COLS)\033[0m"
+        >&2 echo "OnaBashEla.sh requires a minimum of $MIN_ROWS x $MIN_COLS terminal dimention. Try resizing the terminal window :)"
+        exit 3
+    fi
+}
+
+function draw_onaBashEla() {   # draw ona sadhya
     clear;
     tput civis;
 
+    ROWS=$(tput lines)
+    COLS=$(tput cols)
+
+    window_size_checker;
+
     width=39
     padding_left=$((  COLS/2 - width/2 - 11/2 ))
-    padding_top=$(( ROWS/2 - 10 ))
+    padding_top=$(( ROWS/2 - 12 ))
+    c=$padding_top
     if ((padding_top < 0)); then
-        padding_top=2
+        padding_top=0
     fi;
     if ((padding_left < 0)); then
-        padding_left=2
+        padding_left=1
     fi;
-    padding_top_top=$(( padding_top -1 ))
+    padding_init_top=$padding_top
 
     # ela, chor & curry
     (( padding_top ++ ))
@@ -197,16 +247,16 @@ function draw_onaSHadhya() {   # draw ona sadhya
     draw_elipse_item $((padding_left+28)) $((padding_top+3)) 6 3 o 3 ;    # pappadam vannu
 
     # recipe
-    recipe_index;
+    print_recipe;
 
     # onam quote
-    onam_quote $(( padding_top+13 )) ;
+    onam_quote $(( padding_top+12 )) ;
 
     # credit
-    msg="github.com/bRuttaZz/OnaBashEla.sh"
+    msg="[source]"
     msg_="\e]8;;https://github.com/bRuttaZz/OnaBashEla.sh\e\\$msg\e]8;;\e\\"
     tput setaf 7;
-    tput cup $ROWS $((  COLS/2 - ${#msg}/2  ))
+    tput cup 0 $((  COLS - ${#msg} - 1))
     echo -e $msg_
     tput setaf 9;
 
@@ -214,25 +264,33 @@ function draw_onaSHadhya() {   # draw ona sadhya
     # wish
     wish="MERRY ONAM!"
     padding=$((  COLS/2 - ${#wish}/2 ))
-    for counter in $(seq $UPDATE_INTERVEL); do
-        tput setaf ${GREET_COLORS[$(( counter % GREET_COLOR_COUNT ))]}; tput bold;
-        tput cup $padding_top_top $padding;
+    while $g_loop_flag; do
+        ((g_sec_counter ++))
+        if ((g_sec_counter > UPDATE_INTERVEL)); then
+            g_sec_counter=0
+            break;
+        fi
+        tput setaf ${GREET_COLORS[$(( g_sec_counter % GREET_COLOR_COUNT ))]}; tput bold;
+        tput cup $padding_init_top $padding;
         tput smul; echo -n $wish; tput rmul;
 
-        tput cup 1 1 ; tput sgr0; tput setaf 7;
-        echo -n "next quote @ $(( UPDATE_INTERVEL - counter ))s"
+        tput cup 0 1 ; tput sgr0; tput setaf 7;
+        echo -n "next quote @ $(( UPDATE_INTERVEL - g_sec_counter ))s  "
 
         tput setaf 9; tput sgr0;
         sleep 1;
     done;
 
+    # you may wondering why the recursion based rerendering of the whole page..
+    # well, I'm adicted to the slow rendering effects of bash script
+    g_loop_flag=true
+    draw_onaBashEla
 }
 
 function main() {
-    update_row_col
-    while true; do
-        draw_onaSHadhya;
-    done;
+    trap "tput reset; tput cnorm; exit" 2
+    trap 'g_loop_flag=false' WINCH
+    draw_onaBashEla;
 }
 
 # the execution block
